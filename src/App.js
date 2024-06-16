8577861939c9889d5477c8b7e8a19dc055966fc2
hoholyin <holyin97@gmail.com>
2024-06-16 17:16:17 +0800

    set showConfirmation to false for usual page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import RsvpForm from "./RsvpForm";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <span className="app-title">HOL YIN & SHI HUI</span>
-      </header>
-      <body>
+        <header className="App-header">
+            <span className="app-title">HOL YIN & SHI HUI</span>
+            <span className="app-subheader">9 March 2025</span>
+        </header>
+        <body>
         <div className="main">
             <RsvpForm></RsvpForm>
         </div>
--- src/RsvpForm.js
+++ src/RsvpForm.js
@@ -3,6 +3,8 @@ import {useState} from "react";
 import { IoCheckmark } from "react-icons/io5";
 import { RxCross2 } from "react-icons/rx";
 import {postRequest} from "./requestBuilder";
+import { Audio } from 'react-loader-spinner';
+
 const RsvpForm = (props) => {
     const apiEndpoint = "https://hs-backend-69n7.onrender.com/api/rsvp";
 
@@ -10,32 +12,48 @@ const RsvpForm = (props) => {
     const [emailAddress, setEmailAddress] = useState("");
     const [attendanceIndicator, setAttendanceIndicator] = useState("");
     const [diet, setDiet] = useState("");
-
-    const generateAttendanceClassname = (attendanceClassname) => {
-        if (attendanceClassname === "attendance-coming") {
-            const selectedClassname = attendanceIndicator === "Y" ? " selected" : " not-selected";
-            return attendanceClassname + selectedClassname;
-        }
-        if (attendanceClassname === "attendance-not-coming") {
-            const selectedClassname = attendanceIndicator === "N" ? " selected" : " not-selected";
-            return attendanceClassname + selectedClassname;
-        }
-        return "";
-    }
+    const [isLoading, setIsLoading] = useState(false);
+    const [showConfirmation, setShowConfirmation] = useState(false);
 
     const submitRsvp = async () => {
+        setIsLoading(true);
         await postRequest(apiEndpoint, {
             name: name,
             email: emailAddress,
             attendanceIndicator: attendanceIndicator,
             diet: diet
         });
+        setIsLoading(false);
+        setShowConfirmation(true);
     }
 
-    return (
-        <div className="rsvp-form">
-            <div className="form-label-container">
-                <span className="form-label">NAME</span>
+    const canSubmit = () => {
+        return name.length > 0
+            && emailAddress.length > 0
+            && emailAddress.includes("@")
+            && attendanceIndicator.length > 0;
+    }
+
+    const submitAnother = () => {
+        setName("");
+        setEmailAddress("");
+        setAttendanceIndicator("");
+        setDiet("");
+        setShowConfirmation(false);
+    }
+
+    return showConfirmation
+        ? (
+            <div className="confirmation-page">
+                <span className="confirmation-text">Thank you for confirming your attendance!</span>
+                <span className="confirmation-text">We can't wait to see you there.</span>
+                <span className="submit-another" onClick={() => submitAnother()}>SUBMIT ANOTHER NAME</span>
+            </div>
+        )
+        : (
+            <div className="rsvp-form">
+                <div className="form-label-container">
+                    <span className="form-label">NAME</span>
             </div>
             <input className="input-box" type="text" value={name} onChange={e => setName(e.target.value)}/>
             <div className="form-label-container">
@@ -55,10 +73,17 @@ const RsvpForm = (props) => {
                 </div>
             </div>
             <div className="form-label-container">
-                <span className="form-label">PLEASE LET US KNOW YOUR DIETARY RESTRICTIONS</span>
+                <span className="form-label">DIETARY RESTRICTIONS</span>
+                <span className="form-label">(Leave blank if you eat anything)</span>
             </div>
             <input className="input-box" type="text" value={diet} onChange={e => setDiet(e.target.value)}/>
-            <button className="submit" onClick={() => submitRsvp()}>Let's go!</button>
+            {isLoading
+                ? <Audio height={20}/>
+                : <button className={!canSubmit() ? "submit-disabled" : "submit"}
+                          onClick={() => isLoading || !canSubmit() ? null : submitRsvp()}>
+                    Let's go!
+                    </button>
+            }
         </div>
     );
 }
